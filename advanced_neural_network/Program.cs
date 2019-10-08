49a8576e089cbc7f0e03c6adce0efa8efb7e6f36
Gabriella Sinka-Pálinkás <fusewent@gmail.com>
2019-10-08 20:48:33 +0200

    Add project files.

---
+++ advanced_neural_network/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advanced_neural_network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
